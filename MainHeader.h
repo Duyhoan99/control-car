799be65e298bf7bcf1006f20da672ef03c44cad8
Duyhoan99 <duyhoan3905@gmail.com>
2024-12-10 17:54:24 +0700

    Create MainHeader.h

---
+++ MainHeader.h
@@ -0,0 +1,44 @@
+#ifndef MAINHEADER_H  
+#define MAINHEADER_H  
+
+#define SCREEN_WIDTH 90
+#define SCREEN_HEIGHT 26
+#define WIN_WIDTH 70
+
+// Global function declarations
+void modes();
+void gotoxy(int x, int y);
+void instructions();
+void setcursor(bool visible, DWORD size);
+void gameover();
+void getMenu();
+	
+class easyClass {
+	public:
+		static void easyPlay();
+		static void drawEnemy(int ind);
+		static void eraseEnemy(int ind);
+		static void resetEnemy(int ind);
+		static void drawBorder();		
+		static int collision();
+		static void updateScore();	  
+		static void drawCar();
+		static void eraseCar();
+		static void genEnemy(int ind);			
+};
+
+class hardClass {
+	public:
+		static void hardPlay();
+		static void drawEnemy(int ind);
+		static void eraseEnemy(int ind);
+		static void resetEnemy(int ind);
+		static void drawBorder();		
+		static int collision();
+		static void updateScore();	  
+		static void drawCar();
+		static void eraseCar();
+		static void genEnemy(int ind);
+};
+
+#endif // MAINHEADER_H  // Ðóng phan bao ve
